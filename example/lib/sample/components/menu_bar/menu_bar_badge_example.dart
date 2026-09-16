2978beaa669bdaabaf4c97a102daa6a4e49b2544
康兵奎 <178974662@qq.com>
2026-09-16 15:28:27 +0800

    fix: MenuBar 示例页切换标签时内容同步切换

--- example/lib/sample/components/menu_bar/menu_bar_badge_example.dart
+++ example/lib/sample/components/menu_bar/menu_bar_badge_example.dart
@@ -2,8 +2,17 @@ import 'package:santo_ui/santo_ui.dart';
 import 'package:flutter/material.dart';
 
 /// MenuBar 红点与徽标示例
-class MenuBarBadgeExample extends StatelessWidget {
+class MenuBarBadgeExample extends StatefulWidget {
   const MenuBarBadgeExample({Key? key}) : super(key: key);
+
+  @override
+  State<MenuBarBadgeExample> createState() => _MenuBarBadgeExampleState();
+}
+
+class _MenuBarBadgeExampleState extends State<MenuBarBadgeExample> {
+  int _index = 0;
+
+  static const _tabNames = ['首页', '发现', '消息', '我的'];
 
   @override
   Widget build(BuildContext context) {
@@ -20,12 +29,14 @@ class MenuBarBadgeExample extends StatelessWidget {
                 color: const Color(0xFFF5F6FA),
                 borderRadius: BorderRadius.circular(12),
               ),
-              child: Text('消息通知 $i',
+              child: Text('${_tabNames[_index]}内容卡片 $i',
                   style: const TextStyle(fontSize: 14)),
             ),
         ],
       ),
       bottomNavigationBar: SantoMenuBar(
+        currentIndex: _index,
+        onChange: (i) => setState(() => _index = i),
         items: [
           SantoMenuBarItem(
             text: '首页',
